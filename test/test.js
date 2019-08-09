432e9b6696130967da889ec2ae980b37956f73f7
Ajay Kumar <ajay20.kin@gmail.com>
2019-08-09 10:06:01 +0530

    add deployment script

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 var request = require('supertest');
 var app = require('../index.js');
 describe('GET /', function() {
- it('respond with hello world', function(done) {
- //navigate to root and check the the response is "hello world"
- request(app).get('/').expect('hello world', done);
+ it('respond with hello world, this is Ajay Kumar Kommaraju', function(done) {
+ //navigate to root and check the the response is "hello world, this is Ajay Kumar Kommaraju"
+ request(app).get('/').expect('hello world, this is Ajay Kumar Kommaraju', done);
  });
 });
